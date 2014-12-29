d748eb7d64a4110fd20868ff845fe04e50bd2362
Miro Nieminen <miro.nieminen@futurice.com>
2014-12-29 16:46:16 +0200

    Separate the IG images in to a two different listings, put header on the middle

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -6,7 +6,10 @@ function(
 ) {
 
 	// Storages
-	$scope.igImages = [];
+	$scope.igImages = {
+		firstHalf: [],
+		secondHalf: []
+	};
 
 	$scope.init = function initialize() {
 		var scope = $scope;
@@ -14,7 +17,8 @@ function(
 		apiService.getRecentIgPhotos(12)
 		.then(
 			function success(data) {
-				scope.igImages = data.images;
+				scope.igImages.firstHalf = data.images.slice(0,6);
+				scope.igImages.secondHalf = data.images.slice(6,12);
 			}
 		);
 	};
